24e329a079e816ffc4897d7e9b0ae31e73369989
stuart <stuartmarshall@fixie3d.com>
2024-01-11 14:57:50 +0000

    Remove any movies without a poster

--- client/src/store/movieListReducer.ts
+++ client/src/store/movieListReducer.ts
@@ -5,15 +5,18 @@ export const fetchMovies = createAsyncThunk<Movie[], string>(
     'movieList/fetchMovies',
     async (searchRequest) => {
         try {
-            const response: Response = await fetch(`http://localhost:5000/api?search=${searchRequest}`);
+            if(searchRequest === '') return [];
+            const response: Response = await fetch(`/search/${searchRequest}`);
             console.log(response)
             const jsonResponse = await response.json();
-            const data: Movie[] = jsonResponse.map((movie: any) => ({
-                id: movie.id,
-                title: movie.title,
-                overview: movie.overview,
-                release_date: new Date(movie.release_date).toLocaleString('en-UK', { day: 'numeric', month: 'short', year: 'numeric' }),
-                poster_path: `https://image.tmdb.org/t/p/w500/${movie.poster_path}`
+            const data: Movie[] = jsonResponse
+                .filter((movie: any) => movie.poster_path !== null)
+                .map((movie: any) => ({
+                    id: movie.id,
+                    title: movie.title,
+                    overview: movie.overview,
+                    release_date: new Date(movie.release_date).toLocaleString('en-UK', { day: 'numeric', month: 'short', year: 'numeric' }),
+                    poster_path: `https://image.tmdb.org/t/p/w500/${movie.poster_path}`
             }));            
             return data;
         } catch (error) {
