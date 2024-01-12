a43ff8b9ddfa8a59d1090ecde9f24828c60b4ae9
stuart <stuartmarshall@fixie3d.com>
2024-01-12 07:49:01 +0000

    server changes for deployment

--- server/src/app.ts
+++ server/src/app.ts
@@ -42,9 +42,9 @@ app.get("/topRated", limiter, cacheMiddleware, async (req: Request, res: Respons
   }
 });
 
-const port = process.env.PORT || 5000;
-app.listen(port, () => {
-  console.log(`Server is listening on port ${port}`);
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
 });
 
 module.exports = app;
